1177aab039e759cd25a167e6d87d29adf2b6eb4d
Tushar242424 <141857680+Tushar242424@users.noreply.github.com>
2025-11-03 12:46:04 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,7 +1,7 @@
 # Create VPC Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "1.5.7"  
+  version = "6.5.0"  
   # version = "~> 2.78"
 
   # VPC Basic Details
